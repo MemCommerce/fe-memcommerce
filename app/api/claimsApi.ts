295f6fa28896517cfc593d0b571f623c13efc014
tihomirtx88 <tihomirtx88@gmail.com>
2025-08-27 18:20:26 +0300

    Implement claims action

--- app/api/claimsApi.ts
+++ app/api/claimsApi.ts
@@ -1,7 +1,14 @@
+import { ClaimWithItems } from "@/lib/types";
+import { CLAIMS_URL } from "@/lib/urls"
 
 
-export const getProducts = async (): Promise<Product[]> => {
-    const resp = await fetch(PRODUCTS_URL)
-    const data: Product[] = await resp.json()
-    return data
-}
+export const getClaims = async (): Promise<ClaimWithItems[]> => {
+  const resp = await fetch(CLAIMS_URL);
+
+  if (!resp.ok) {
+    throw new Error("Failed to fetch claims");
+  }
+
+  const data: ClaimWithItems[] = await resp.json();
+  return data;
+};
