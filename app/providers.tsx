8960abfd4eb147580591440f5a123b1873e935d3
tihomirtx88 <tihomirtx88@gmail.com>
2025-09-01 15:53:28 +0300

    Fix bug with hydration error in Next.js

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,19 +5,16 @@ import type React from "react";
 import { CartProvider } from "@/hooks/useCart";
 import { WishlistProvider } from "@/hooks/useWishlist";
 import AuthProvider from "@/context/AuthProvider";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </NextThemesProvider>
-  );
-}
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
       <AuthProvider>
         <CartProvider>
           <WishlistProvider>{children}</WishlistProvider>
