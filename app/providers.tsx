f2f59723d1f8e8fba13733dde35571c4dab72b03
tihomirtx88 <tihomirtx88@gmail.com>
2025-09-01 11:23:14 +0300

    Adding theme provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,16 +2,19 @@
 
 import type React from "react";
 
+import { ThemeProvider } from "next-themes";
 import { CartProvider } from "@/hooks/useCart";
 import { WishlistProvider } from "@/hooks/useWishlist";
 import AuthProvider from "@/context/AuthProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <CartProvider>
-        <WishlistProvider>{children}</WishlistProvider>
-      </CartProvider>
-    </AuthProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <AuthProvider>
+        <CartProvider>
+          <WishlistProvider>{children}</WishlistProvider>
+        </CartProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
