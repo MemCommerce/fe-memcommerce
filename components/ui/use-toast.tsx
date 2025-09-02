cc1d9825408bd854162decc1875e29adaa9d0904
tihomirtx88 <tihomirtx88@gmail.com>
2025-09-02 12:40:16 +0300

    Refactoring useToast

--- components/ui/use-toast.tsx
+++ components/ui/use-toast.tsx
@@ -1,16 +1,29 @@
 "use client";
 
 import * as React from "react";
-import { Toaster, toast as shadToast, ToastProps } from "@/components/ui/toast"; 
+import { Toast, ToastProvider, ToastViewport, ToastProps } from "@/components/ui/toast";
 
+interface ToastItem extends ToastProps {
+  id: string;
+}
 
 export const useToast = () => {
-  const toast = React.useCallback(
-    (props: ToastProps) => {
-      shadToast(props);
-    },
-    []
+  const [toasts, setToasts] = React.useState<ToastItem[]>([]);
+
+  const toast = React.useCallback((props: Omit<ToastItem, "id">) => {
+    const id = crypto.randomUUID();
+    setToasts((prev) => [...prev, { ...props, id }]);
+    setTimeout(() => setToasts((prev) => prev.filter((t) => t.id !== id)), 4000); // auto dismiss
+  }, []);
+
+  const ToastContainer = () => (
+    <ToastProvider>
+      {toasts.map((t) => (
+        <Toast key={t.id} {...t} />
+      ))}
+      <ToastViewport />
+    </ToastProvider>
   );
 
-  return { toast };
+  return { toast, ToastContainer };
 };
