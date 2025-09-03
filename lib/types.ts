38a5402a344e87fe63b9ea1fd3462c54bf940281
tihomirtx88 <tihomirtx88@gmail.com>
2025-09-03 14:33:51 +0300

    Moving interfaces

--- lib/types.ts
+++ lib/types.ts
@@ -245,27 +245,3 @@ export type ClaimWithItems = {
 };
 
 
-// PRoduct revierews page 
-export interface ProductReviewsProps {
-  reviews: SFProductWithReview["reviews"];
-  getVariantForReview: (variantId: string) => SFProductWithReview["variants"][0] | undefined;
-  averageRating: number;
-}
-
-// Product options props 
-export interface ProductOptionsProps {
-  product: SFProductWithReview;
-  selectedVariant: StorefrontVariant;
-  setSelectedVariant: React.Dispatch<React.SetStateAction<StorefrontVariant>>;
-  quantity: number;
-  setQuantity: React.Dispatch<React.SetStateAction<number>>;
-}
-
-// Product info props 
-export interface ProductInfoProps {
-  name: string;
-  description: string;
-  categoryName: string;
-  price: number;
-  id: string;
-}
